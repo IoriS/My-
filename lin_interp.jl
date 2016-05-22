b3e8c11fec0075a1445f7ef924b4ebc1d18b5ff0
unknown <>
2016-05-22 15:31:33 +0900

    gridの最後の要素を入れた時に0を返す仕様を修正しました。

--- lin_interp.jl
+++ lin_interp.jl
@@ -5,8 +5,13 @@ end
 
 function Base.call(points::my_lin_interp,x::Real)
     i = searchsortedlast(grid,x)
-    if i == 0 || i == length(grid)
+    k = searchsortedfirst(grid,x)
+    if i == 0 || (i == length(grid) && k == length(grid) + 1)
         return 0
+    end
+
+    if i == length(grid) && k != length(grid) + 1
+        return (vals[i])
     end
     
     interpolated_value = (x - grid[i])*(vals[i+1]-vals[i])/(grid[i+1]-grid[i])+vals[i]
@@ -18,9 +23,12 @@ function Base.call{T<:Real}(points::my_lin_interp,x::AbstractVector{T})
     out = Array(Float64, n)
     for l in 1:n
         i = searchsortedlast(grid,x[l])
-        if i == 0 || i == length(grid)
-            out[l] = 0
-        else
+        k = searchsortedfirst(grid,x[l])
+        if i == 0 || (i == length(grid) && k == length(grid)+1)
+            out[l] = 0 
+        elseif i == length(grid) && k != length(grid)+1
+            out[l] = vals[i]
+        else 
             out[l] = (x[l] - grid[i])*(vals[i+1]-vals[i])/(grid[i+1]-grid[i])+vals[i]
         end
     end
