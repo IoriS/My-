aa78fd409b33dc509373b2ce3ee5a7efd70d6447
unknown <>
2016-06-16 01:15:12 +0900

    henkousimasita

--- deferred_acceptance.jl
+++ deferred_acceptance.jl
@@ -3,19 +3,21 @@ function deferred_acceptance(m_prefs,f_prefs)
     n = size(f_prefs, 2)
     m_matched=zeros(Int64,m)
     f_matched=zeros(Int64,n)
-    for d in 1:n+1
-        for h in 1:m
-            if m_matched[h]==0
-                if m_prefs[d,h] == 0
+    m_counter=zeros(Int64,m)　
+    for d in 1:n+1 
+        for h in 1:m 
+            if m_matched[h] == 0
+                if m_prefs[m_counter[h]+1,h]==0
                     m_matched[h] = 0
-                    elseif findfirst(f_prefs[:,m_prefs[d,h]],h) > findfirst(f_prefs[:,m_prefs[d,h]],f_matched[m_prefs[d,h]])
-                    if f_matched[m_prefs[d,h]] != 0
-                        m_matched[f_matched[m_prefs[d,h]]] = 0
-                    end
-                    m_matched[h]=m_prefs[d,h]
-                    f_matched[m_prefs[d,h]]=h
-                else m_matched[h]=0
-                end 
+                elseif findfirst(f_prefs[:,m_prefs[m_counter[h]+1,h]],h) < findfirst(f_prefs[:,m_prefs[m_counter[h]+1,h]],f_matched[m_prefs[m_counter[h]+1]])　
+                    if f_matched[m_prefs[m_counter[h]+1,h]] != 0
+                        m_matched[f_matched[m_prefs[m_counter[h]+1,h]]]=0　
+                        m_counter[f_matched[m_prefs[m_counter[h]+1,h]]] += 1 
+                    end        
+                    m_matched[h]=m_prefs[m_counter[h]+1,h]　
+                    f_matched[m_prefs[m_counter[h]+1,h]]=h　
+                else m_counter[h] += 1 
+                end
             end
         end
     end
